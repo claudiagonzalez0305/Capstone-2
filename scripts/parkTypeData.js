d0486f0910b68ce878c5afc2337db3c96eee5b5b
Claudia Gonzalez <CGonzalez@appdev.yearup.com>
2023-05-26 12:38:57 -0400

    update code

--- scripts/parkTypeData.js
+++ scripts/parkTypeData.js
@@ -1,15 +1,37 @@
+"use strict";
+
+window.onload = init;
+
 const parkTypesArray = [
-    "National Park",
-    "National Monument",
-    "Recreation Area",
-    "Scenic Trail",
-    "Battlefield",
-    "Historic",
-    "Memorial",
-    "Preserve",
-    "Island",
-    "River",
-    "Seashore",
-    "Trail",
-    "Parkway"
-]
+  "National Park",
+  "National Monument",
+  "Recreation Area",
+  "Scenic Trail",
+  "Battlefield",
+  "Historic",
+  "Memorial",
+  "Preserve",
+  "Island",
+  "River",
+  "Seashore",
+  "Trail",
+  "Parkway",
+];
+
+function init() {
+  console.log("parkTypeData.js");
+  fillParkTypeDropdown();
+  const searchBtn = document.getElementById("searchBtn");
+  searchBtn.onclick = searchBtnOnClick;
+}
+
+
+
+function searchBtnOnClick() {
+  const selectPark = document.getElementById("selectPark");
+  let selectedParkByUser = selectPark.value;
+
+  let message = `You selected ${selectedParkByUser}.`;
+  const parkInfo = document.getElementById("state&ParkInfo");
+  parkInfo.innerHTML = message;
+}
